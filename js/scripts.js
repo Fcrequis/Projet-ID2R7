4979d32d618c0456bc899b3b4e51dd3f7cf83eac
Oliver <oli.spicer@gmail.com>
2014-07-14 14:25:06 +0200

    Mis a jour Lundi

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ $(document).on("pagecreate",function(event){
 	// Enleve les fleches des elements <li>
 	var bool = $('.ui-btn').hasClass('ui-btn-icon-right');
 	if(bool){$('.ui-btn').removeClass('ui-btn-icon-right');}
+
+	$.mobile.page.prototype.options.domCache = true;
 	
 	//Redirige vers d'autres pages
 		//Accueil - *
